f646af568ca21d584c415c248287af159855abd8
Meirdavv <meirbusinessac@gmail.com>
2024-06-28 15:01:28 +0300

    finished search_books without implementing functions

--- ui/search_books.py
+++ ui/search_books.py
@@ -1,24 +1,47 @@
 import api_functions
-def search_books():       
-    query = input("Enter a search query: ")
-    list_of_books_results = api_functions.search_books(query) #search_books function is needed. return here a list of book objects. randomize a price for the book object
-    for book in list_of_books_results:
-        print(book) #Book.__str__() function is needed. return here each book with its details
+import db_functions
+import classes.library as library
 
-    to_add_to_inventory = input("Would you like to add any of the books here to your inventory? 'y'/'n': ")
-    if to_add_to_inventory == 'y':
-        book_id = input("Please provide the desired book id: ")
-        amount = int(input("Please provide an amount: "))
 
-        #Check if book exists in search results
-        is_book_id_exist = False
-        for book in list_of_books_results:
-            if book.book_id == book_id:
-                is_book_id_exist = True
 
-            
 
-        #Check if library budget can afford ordering the books
-        can_library_afford = l
+def search_books():  
 
-        #Add the books to the library. Check first if book exists. if so- change the book availability amount. if not- create a new book and append. Make sure to update the library budget
+    inventory_json = db_functions.get_table_as_json(table_name = 'books') #need to implement
+    inventory = library.from_dict(inventory_json)
+
+    amount_of_search_results = 10   
+    print('(1) Search books')  
+    query = input("\t- search for a book by title: ")
+    books_results = api_functions.search_books(query , amount_of_search_results) #search_books function is needed. return here a list of book objects. randomize a price for the book object
+    
+    print(f'\t\t-- Top {amount_of_search_results} books matching your search results: ')
+    print(f'\t\t\tID | TITLE')
+    for id,book in enumerate(books_results):
+        print(f'[{id + 1}]| {book.title}')
+
+    book_id = input('\t\t-- To add a book to the inventory, enter the id: ')
+    
+    try:
+        book_id = int(book_id)
+        if book_id not in range(1,11):
+            raise ValueError
+        else:
+            selected_book = books_results[book_id-1]
+        
+    except ValueError:
+        print('Invalid Input! A number between 1-10 must be provided. Try again...')
+        search_books()
+    
+    amount_of_books = input('\t\t-- How many copies do you want to add to the inventory?: ')
+
+    try:
+        amount_of_books = int(amount_of_books)
+
+    except ValueError:
+        print('Invalid Input! A number must be provided. Try again...')
+        search_books()
+
+    to_add = input(f'Confirm adding {amount_of_books} copies of {selected_book.get_title()} to inventory (y/n): ') 
+    if to_add == 'y':
+        inventory.add_book(selected_book) # Need to implement - create new book or change stock if already exists
