d96744ea269c4d627f88dda2c931fb46ad730ef7
Meirdavv <meirbusinessac@gmail.com>
2024-06-29 19:42:29 +0300

    improved api input to prevent missing fields,fixed adding book to table function to allow updating a book if its already existant instead of appending a new book, and overally organized the code at search_books

--- db_functions.py
+++ db_functions.py
@@ -40,21 +40,48 @@ def get_table_as_json_array(table_name : str) -> list[dict]:
 
 def add_book_to_books_table(book_data:dict) -> None:
     table_name = 'books'
+    book_id_field = 'book_id'
+    amount_available_field = 'amount_available'
+    amount_total_field = 'amount_total'
     connection, cursor = create_connection()
 
-    insert_query = psycopg2.sql.SQL("""
-    INSERT INTO books (
-        book_id, title, description, authors, genre, published_date, language, 
-        page_count, cover_image_url, publisher, avg_rating, maturity_rate, 
-        buying_price, selling_price, amount_total, amount_available
-    ) VALUES (
-        %(book_id)s, %(title)s, %(description)s, %(authors)s, %(genre)s, %(published_date)s, %(language)s, 
-        %(page_count)s, %(cover_image_url)s, %(publisher)s, %(avg_rating)s, %(maturity_rate)s, 
-        %(buying_price)s, %(selling_price)s, %(amount_total)s, %(amount_available)s
-    )
-""")
-    
-    cursor.execute(insert_query,book_data)
-    connection.commit()
+    #get the current amount_available value of the book
+    select_query = f"SELECT {amount_available_field} FROM {table_name} WHERE {book_id_field} = %s"
+    cursor.execute(select_query,(book_data['book_id'],))
+    rows = (cursor.fetchall())
+
+    if rows: #if book exists in table
+        #calculate amount_available:
+        old_amount_available = rows[0][0]
+        new_amount_available = old_amount_available + book_data['amount_available']
+
+        #calculate amount_total:
+        select_query = f"SELECT {amount_total_field} FROM {table_name} WHERE {book_id_field} = %s"
+        cursor.execute(select_query,(book_data['book_id'],))
+        rows = (cursor.fetchall())
+        old_amount_total = rows[0][0]
+        new_amount_total = old_amount_total + book_data['amount_total']
+
+        #update new values in table
+        update_query = f"UPDATE {table_name} SET {amount_available_field} = %s, {amount_total_field} = %s WHERE {book_id_field} = %s"
+        cursor.execute(update_query,(new_amount_available,new_amount_total,book_data['book_id']))
+        connection.commit()
+        
+    else:
+        #if book doesn't exist in table, add a new book
+        insert_query = psycopg2.sql.SQL(f"""
+        INSERT INTO {table_name} (
+            book_id, title, description, authors, genre, published_date, language, 
+            page_count, cover_image_url, publisher, avg_rating, maturity_rate, 
+            buying_price, selling_price, amount_total, amount_available
+        ) VALUES (
+            %(book_id)s, %(title)s, %(description)s, %(authors)s, %(genre)s, %(published_date)s, %(language)s, 
+            %(page_count)s, %(cover_image_url)s, %(publisher)s, %(avg_rating)s, %(maturity_rate)s, 
+            %(buying_price)s, %(selling_price)s, %(amount_total)s, %(amount_available)s
+        )
+    """)
+        cursor.execute(insert_query,book_data)
+        connection.commit()
+
     cursor.close()
     connection.close()
--- ui/search_books.py
+++ ui/search_books.py
@@ -1,6 +1,6 @@
 import api_functions
 import db_functions
-import classes.library as library
+from classes.library import Library
 from classes.book import Book
 
  
@@ -8,44 +8,76 @@ from classes.book import Book
 
 def search_books():  
 
-
-    amount_of_search_results = 10   
-    print('\n\t(1) Search books')  
-    query = input("\t- search for a book by title: ")
-    search_results = api_functions.search_books(query , amount_of_search_results)  
-
-    print(f'\t-- Top {amount_of_search_results} books matching your search results: ')
-    print(f'\tID | TITLE')
-    for id,book in enumerate(search_results):
-        print(f'\t[{id + 1}]| {book['title']}')
-
+    print(
+        '''
+        Search books
+        ------------''')
     
-    try:
-        book_id = int(input('\t-- To add a book to the inventory, enter the id: '))
-        if book_id not in range(1,11):
-            raise ValueError
-        else:
-            book_json = search_results[book_id-1]
-            book_obj = Book.from_dict(book_json)
+    query = input("\tSearch a book by title: ")
         
-    except ValueError:
-        print('Invalid Input! A number between 1-10 must be provided. Try again...')
-        search_books()
+    search_results = api_functions.search_books(query, 10)
+
+    if search_results:
+        display_search_results(search_results)
+
+        id = int(input("\n\tTo add a book to the inventory, enter the id (1-10): "))
+        user_selected_book_json = search_results[id-1]
+
+        amount = int(input("\tHow many copies do you want to add to the inventory?: "))
+        user_selected_book_json['amount_total'] = user_selected_book_json['amount_available'] = amount
+
+        to_add = input(f"\tConfirm adding {amount} copies of {user_selected_book_json['title']} to inventory (y/n): ")
+        if (to_add == 'y'):
+            db_functions.add_book_to_books_table(user_selected_book_json)
+            print("\tBook was successfuly added.")
+
+
+
+def display_search_results(search_results :list[dict]) -> None:
+    print(f"\tTop {len(search_results)} books matching your results: ")
+    print("\tID | TITLE")
+    for id,result in enumerate(search_results):
+        print(f"\t[{id+1}]| {result['title']}") #id starts with 0, so changed it to id+1
+
     
-    amount_of_books = input('\t-- How many copies do you want to add to the inventory?: ')
+    # print('\n\t(1) Search books')  
+    # query = input("\t- search for a book by title: ")
 
-    try:
-        amount_of_books = int(amount_of_books)
+    # amount_of_search_results = 10   
+    # search_results = api_functions.search_books(query , amount_of_search_results)  
 
-    except ValueError:
-        print('\tInvalid Input! A number must be provided. Try again...')
-        search_books()
+    # print(f'\t-- Top {amount_of_search_results} books matching your search results: ')
+    # print(f'\tID | TITLE')
+    # for id,book in enumerate(search_results):
+    #     print(f'\t[{id + 1}]| {book['title']}')
 
-    to_add = input(f'\tConfirm adding {amount_of_books} copies of {book_obj.get_title()} to inventory (y/n): ') 
-    if to_add == 'y':
-        inventory_json = db_functions.get_table_as_json_array(table_name = 'books') 
-        inventory = library.Library.from_dict(inventory_json)
-        inventory.add_book(book_obj,amount_of_books)
-        print('\n\t\t Book was added successfully!\n')
-        print(inventory)
-        db_functions.add_book_to_books_table(book_json)
+
+    # try:
+    #     book_id = int(input('\t-- To add a book to the inventory, enter the id: '))
+    #     if book_id not in range(1,11):
+    #         raise ValueError
+    #     else:
+    #         book_json = search_results[book_id-1]
+    #         book_obj = Book.from_dict(book_json)
+        
+    # except ValueError:
+    #     print('Invalid Input! A number between 1-10 must be provided. Try again...')
+    #     search_books()
+
+    # amount_of_books = input('\t-- How many copies do you want to add to the inventory?: ')
+
+    # try:
+    #     amount_of_books = int(amount_of_books)
+
+    # except ValueError:
+    #     print('\tInvalid Input! A number must be provided. Try again...')
+    #     search_books()
+
+    # to_add = input(f'\tConfirm adding {amount_of_books} copies of {book_obj.get_title()} to inventory (y/n): ') 
+    # if to_add == 'y':
+    #     inventory_json = db_functions.get_table_as_json_array(table_name = 'books') 
+    #     inventory = library.Library.from_dict(inventory_json)
+    #     inventory.add_book(book_obj,amount_of_books)
+    #     print('\n\t\t Book was added successfully!\n')
+    #     print(inventory)
+    #     db_functions.add_book_to_books_table(book_json)
