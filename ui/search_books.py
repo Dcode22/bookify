c9d4b5884e803a67ddaba5bfc0af063a6bc9a04d
Meirdavv <meirbusinessac@gmail.com>
2024-06-29 15:08:20 +0300

    fixed layout design on search books menu

--- ui/search_books.py
+++ ui/search_books.py
@@ -10,18 +10,18 @@ def search_books():
 
 
     amount_of_search_results = 10   
-    print('\n(1) Search books')  
+    print('\n\t(1) Search books')  
     query = input("\t- search for a book by title: ")
     search_results = api_functions.search_books(query , amount_of_search_results)  
 
-    print(f'\t\t-- Top {amount_of_search_results} books matching your search results: ')
-    print(f'\t\tID | TITLE')
+    print(f'\t-- Top {amount_of_search_results} books matching your search results: ')
+    print(f'\tID | TITLE')
     for id,book in enumerate(search_results):
-        print(f'\t\t[{id + 1}]| {book['title']}')
+        print(f'\t[{id + 1}]| {book['title']}')
 
     
     try:
-        book_id = int(input('\t\t-- To add a book to the inventory, enter the id: '))
+        book_id = int(input('\t-- To add a book to the inventory, enter the id: '))
         if book_id not in range(1,11):
             raise ValueError
         else:
@@ -32,16 +32,16 @@ def search_books():
         print('Invalid Input! A number between 1-10 must be provided. Try again...')
         search_books()
     
-    amount_of_books = input('\t\t-- How many copies do you want to add to the inventory?: ')
+    amount_of_books = input('\t-- How many copies do you want to add to the inventory?: ')
 
     try:
         amount_of_books = int(amount_of_books)
 
     except ValueError:
-        print('\t\tInvalid Input! A number must be provided. Try again...')
+        print('\tInvalid Input! A number must be provided. Try again...')
         search_books()
 
-    to_add = input(f'\t\tConfirm adding {amount_of_books} copies of {book_obj.get_title()} to inventory (y/n): ') 
+    to_add = input(f'\tConfirm adding {amount_of_books} copies of {book_obj.get_title()} to inventory (y/n): ') 
     if to_add == 'y':
         inventory_json = db_functions.get_table_as_json_array(table_name = 'books') 
         inventory = library.Library.from_dict(inventory_json)
