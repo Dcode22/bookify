99d6a35f1f2ea19517ee51d360c4616177f2e6a1
Dovid Levine <dovidlevine@Dovids-MacBook-Pro.local>
2024-06-30 13:06:32 +0300

    major refactor and upgrade to manage_customers functionality

--- ui/manage_customers.py
+++ ui/manage_customers.py
@@ -33,101 +33,187 @@ Enter an option:
 
 
 def list_all_customers():
-    conn = psycopg2.connect(**db_params)
-    cur = conn.cursor()
-    query = "SELECT * FROM customers"
-    cur.execute(query)
-    rows = cur.fetchall()
-    table_headers = ["CUSTOMER ID", "FIRST NAME", "LAST NAME", "EMAIL", "PHONE"]
-    print_table(table_headers, rows)
-    cur.close()
-    conn.close()
-    manage_customers()
+    try:
+        conn = psycopg2.connect(**db_params)
+        cur = conn.cursor()
+        query = "SELECT * FROM customers"
+        cur.execute(query)
+        rows = cur.fetchall()
+        table_headers = ["CUSTOMER ID", "FIRST NAME", "LAST NAME", "EMAIL", "PHONE"]
+        print_table(table_headers, rows)
+        cur.close()
+        conn.close()
+        manage_customers()
+    except Exception as e:
+        print('Error: ', e)
 
 def search_customers():
-    conn = psycopg2.connect(**db_params)
-    cur = conn.cursor()
-    search = input("Search customers: ")
-    query = f"SELECT * FROM customers WHERE LOWER(CONCAT(first_name, ' ', last_name, ' ', email)) LIKE LOWER('%{search}%');"
-    cur.execute(query)
-    rows = cur.fetchall()
-    table_headers = ["CUSTOMER ID", "FIRST NAME", "LAST NAME", "EMAIL", "PHONE"]
-    print_table(table_headers, rows)
-    cur.close()
-    conn.close()
+    try:
+        conn = psycopg2.connect(**db_params)
+        cur = conn.cursor()
+        search = input("SEARCH CUSTOMERS (type 'x' to cancel): ")
+        if search == 'x':
+            return manage_customers()
+        query = f"SELECT * FROM customers WHERE LOWER(CONCAT(first_name, ' ', last_name, ' ', email)) LIKE LOWER('%{search}%');"
+        cur.execute(query)
+        rows = cur.fetchall()
+        table_headers = ["CUSTOMER ID", "FIRST NAME", "LAST NAME", "EMAIL", "PHONE"]
+        print_table(table_headers, rows)
+        cur.close()
+        conn.close()
+        if len(rows) == 0:
+            print('NO CUSTOMERS MATCHED YOUR SEARCH')
+            return search_customers()
 
+        select_customer_by_id(rows)
+    except Exception as e:
+        print('Error:', e)
+        print('Try again')
+        search_customers()
+
+
+def select_customer_by_id(rows):
+    global updated_customer_obj
     customer_id = int(input("Select a customer by ID:"))
-    customer = [customer for customer in rows if customer[0] == customer_id][0]
-    if customer:
-        print(f"{customer[1]} {customer[2]}".upper())
-        choice = input('''
+    matching_customers = [customer for customer in rows if customer[0] == customer_id]
+    if len(matching_customers):
+        customer = matching_customers[0]
+        updated_customer_obj = {
+            'first_name': customer[1],
+            'last_name': customer[2],
+            'email': customer[3],
+            'phone': customer[4]
+        }
+        choice = input(f'''
+CUSTOMER: {f"{customer[1]} {customer[2]}".upper()}
 select an option:
-(d) delete user
-(u) update user
-''')
+(d) delete customer
+(u) update customer
+(x) cancel
+
+    ''')
+
         if choice == 'd':
-            confirmation = input(f'To confirm that you want to permanently delete the customer {customer[1]} {customer[2]}, type yes, otherwise type x:')
-            if confirmation == 'yes':
-                conn = psycopg2.connect(**db_params)
-                cur = conn.cursor()
-                query = "DELETE FROM customers WHERE customer_id = %s;"
-                cur.execute(query, (customer[0], ))
-                conn.commit()
-                print(cur.statusmessage)
-                cur.close()
-                conn.close()
-                manage_customers()
-            else:
-                manage_customers()
+            delete_customer(customer)
         elif choice == 'u':
-            choice = input('''
+            update_customer(customer)
+        elif choice == 'x':
+            search_customers()
+    else:
+        print('INVALID SELECTION, TRY AGAIN')
+        select_customer_by_id(rows)
+
+
+
+def delete_customer(customer):
+    try: 
+        confirmation = input(f'To confirm that you want to permanently delete the customer {customer[1]} {customer[2]}, type yes, otherwise type x:')
+        if confirmation == 'yes':
+            conn = psycopg2.connect(**db_params)
+            cur = conn.cursor()
+            query = "DELETE FROM customers WHERE customer_id = %s;"
+            cur.execute(query, (customer[0], ))
+            conn.commit()
+            print(cur.statusmessage)
+            cur.close()
+            conn.close()
+            manage_customers()
+        else:
+            manage_customers()
+    except Exception as e:
+        print('Error:', e)
+        search_customers()
+
+updated_customer_obj = {}
+
+def update_customer(customer):
+    print('update customer func')
+    print(f'''
+          
+
+CUSTOMER
+---------------
+FIRST NAME: {updated_customer_obj["first_name"]} 
+LAST NAME: {updated_customer_obj["last_name"]} 
+EMAIL: {updated_customer_obj["email"]} 
+PHONE: {updated_customer_obj["phone"]}''')
+    choice = input('''
 (1) Update first name
 (2) Update last name
 (3) Update email address
 (4) Update phone number
+(x) Cancel
+
 ''')
-            choices = ['first_name', 'last_name', 'email', 'phone']
-            new_value = ''
-            if choice == '1':
-                new_value = input(f'Enter new first name for {customer[1]} {customer[2]}: ')
-            elif choice == '2':
-                new_value = input(f'Enter new last name for {customer[1]} {customer[2]}: ')
-            elif choice == '3':
-                new_value = input(f'Enter new email for {customer[1]} {customer[2]}: ')
-            elif choice == '4':
-                new_value = input(f'Enter new phone number for {customer[1]} {customer[2]}: ')
-            else:
-                print('Invalid option')
-                search_customers()
-            confirmation = input(f"Are you sure you want to update the {choices[int(choice)-1]} for {customer[1]} {customer[2]} to {new_value}? (y/n)")
-            if confirmation == 'y':
-                conn = psycopg2.connect(**db_params)
-                cur = conn.cursor()
-                query = f"UPDATE customers SET {choices[int(choice)-1]} = %s WHERE customer_id = {customer[0]};"
-                cur.execute(query, (new_value, ))
-                conn.commit()
-                print(cur.statusmessage)
-                cur.close()
-                conn.close()
-                search_customers()
+    choices = ['first_name', 'last_name', 'email', 'phone']
+    new_value = ''
+    if choice == '1':
+        new_value = input(f'Enter new first name for {customer[1]} {customer[2]}: ')
+    elif choice == '2':
+        new_value = input(f'Enter new last name for {customer[1]} {customer[2]}: ')
+    elif choice == '3':
+        new_value = input(f'Enter new email for {customer[1]} {customer[2]}: ')
+    elif choice == '4':
+        new_value = input(f'Enter new phone number for {customer[1]} {customer[2]}: ')
+    elif choice == 'x':
+        manage_customers()
+    else:
+        print('Invalid option')
+        search_customers()
+    confirmation = input(f"Are you sure you want to update the {choices[int(choice)-1]} for {customer[1]} {customer[2]} to {new_value}? (y/n)")
+    if confirmation == 'y':
+        try:
+            conn = psycopg2.connect(**db_params)
+            cur = conn.cursor()
+            query = f"UPDATE customers SET {choices[int(choice)-1]} = %s WHERE customer_id = {customer[0]};"
+            cur.execute(query, (new_value, ))
+            conn.commit()
+            print(cur.statusmessage)
+            cur.close()
+            conn.close()
+            updated_customer_obj[choices[int(choice)-1]] = new_value
+            update_customer(customer)
+        except Exception as e:
+            print('Error:', e)
+            print('Try again')
+            update_customer(customer)
+
+
 
 def new_customer():
-    print("CREATE NEW CUSTOMER")
-    first_name = input("Enter first name: ")
-    last_name = input("Enter last name: ")
-    email = input("Enter email: ")
-    phone = input("Enter phone: ")
+    print('''
+CREATE NEW CUSTOMER
+-------------------
+
+''')
+    first_name = input("Enter first name (enter 'x' to cancel): ")
+    if first_name == 'x':
+        manage_customers()
+    last_name = input("Enter last name (enter 'x' to cancel): ")
+    if last_name == 'x':
+        manage_customers()
+    email = input("Enter email (enter 'x' to cancel): ")
+    if email == 'x':
+        manage_customers()
+    phone = input("Enter phone (enter 'x' to cancel): ")
+    if phone == 'x':
+        manage_customers()
     print(f"FIRST NAME: {first_name}, LAST NAME: {last_name}, EMAIL: {email}, PHONE: {phone}")
     confirmation = input("Are you sure you want to create this customer? (y/n)")
     if confirmation == 'y':
-        conn = psycopg2.connect(**db_params)
-        cur = conn.cursor()
-        query = "INSERT INTO customers (first_name, last_name, email, phone) VALUES (%s, %s, %s, %s)"
-        cur.execute(query, (first_name, last_name, email, phone))
-        conn.commit()
-        print(cur.statusmessage)
-        cur.close()
-        conn.close()
-        manage_customers()
+        try:
+            conn = psycopg2.connect(**db_params)
+            cur = conn.cursor()
+            query = "INSERT INTO customers (first_name, last_name, email, phone) VALUES (%s, %s, %s, %s)"
+            cur.execute(query, (first_name, last_name, email, phone))
+            conn.commit()
+            print(cur.statusmessage)
+            cur.close()
+            conn.close()
+            manage_customers()
+        except Exception as e:
+            print('Error creating user:', e)
+            print('Try again')
+            new_customer()
     else:
         manage_customers()
